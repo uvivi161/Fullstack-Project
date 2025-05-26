6b6d30707a87d259b6fa276394f39f7306aa8197
uvivi161 <uvi69161@gmail.com>
2025-05-26 16:31:21 +0300

    angular-change static.json

--- client-angular/src/app/app.routes.ts
+++ client-angular/src/app/app.routes.ts
@@ -1,16 +1,11 @@
 import { Routes } from '@angular/router';
-// import { HomePageComponent } from '../components/home-page/home-page.component';
-// import { UsersManegmant } from '../components/users-manegmant/users-manegmant.component';
 import { UsersManegmantComponent } from '../components/users-manegmant/users-manegmant.component';
-// import { CityYearlyChartComponent } from '../components/graph/graph.component';
 import { authGuard } from '../gaurds/auth.guard';
 import { HomeComponent } from '../components/home-page/home-page.component';
 import { DashboardLayoutComponent } from '../components/dashboard-layout/dashboard-layout.component';
-// import { DashboardHomeComponent } from '../components/dashboard-home/dashboard-home.component';
 import { UserProfileComponent } from '../components/user-profile/user-profile.component';
 import { CityChartsDashboardComponent } from '../components/graph/graph.component';
 import { DashboardHomeComponent } from '../components/dashboard-home/dashboard-home.component';
-// import { DashboardHomeComponent } from '../components/dashboard-home/dashboard-home.component';
 
 export const routes: Routes = [
     {path:'', component:HomeComponent},
